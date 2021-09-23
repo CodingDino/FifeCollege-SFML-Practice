41a8cc2a01cb1880b347665c6c86ddc123823a62
SarahHerzog <sarahherzog@fife.ac.uk>
2021-09-23 12:21:42 +0100

    Moved non-graphic assets to correct subfolders, added example text functionality

--- SFML Practice Project/Main.cpp
+++ SFML Practice Project/Main.cpp
@@ -3,8 +3,7 @@
 int main()
 {
     // Window Setup
-    sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    sf::RenderWindow window(desktop, "SFML Practice", sf::Style::None);
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "SFML Practice", sf::Style::Titlebar);
 
     // Load texture
     sf::Texture playerTexture;
@@ -14,6 +13,26 @@ int main()
     sf::Sprite playerSprite;
     playerSprite.setTexture(playerTexture);
 
+    // Load font
+    sf::Font mainFont;
+    mainFont.loadFromFile("Assets/Fonts/Dogica.ttf");
+
+    // Setup Text
+    sf::Text titleText;
+    titleText.setFont(mainFont);
+    titleText.setString("Hello world!");
+    titleText.setPosition(window.getSize().x/2, window.getSize().y/2);
+    titleText.setFillColor(sf::Color(255, 166, 0));
+    titleText.setCharacterSize(100);
+    titleText.setStyle(sf::Text::Italic | sf::Text::Underlined);
+
+    sf::FloatRect textRect = titleText.getLocalBounds();
+    titleText.setOrigin(textRect.left + textRect.width/2, textRect.top + textRect.height/2);
+
+
+    titleText.setString("Hello");
+    textRect = titleText.getLocalBounds();
+    titleText.setOrigin(textRect.left + textRect.width / 2, textRect.top + textRect.height / 2);
 
 
     while (window.isOpen())
@@ -25,8 +44,11 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
 
+
+            // Is the event a key press event
             if (event.type == sf::Event::KeyPressed)
             {
+                // Is the event specifically related to the escape key
                 if (event.key.code == sf::Keyboard::Escape)
                 {
                     window.close();
@@ -43,6 +65,7 @@ int main()
         window.clear();
 
         // Draw everything
+        window.draw(titleText);
         window.draw(playerSprite);
 
         window.display();
